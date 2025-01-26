f334d7be0c35077b3e7c889e9791355bcd5b283c
mustafo <mustafo@gmail.com>
2025-01-26 18:46:11 +0500

    in coment

---
+++ app.js
@@ -0,0 +1,406 @@
+// if 1
+// let son = 3
+// if(son > 0) {
+//      son++
+// }else{
+//      son--
+// }
+// console.log(son);4
+// if 2
+// let a = -3
+// let b = 2
+// let c = 1
+// if (a>0&&b>0&&c>0){
+//      console.log("3ta musbat son");
+// }else if(a<0&&b>0&&c>0 || a>0&&b<0&&c>0 ||a>0&&b>0&&c<0) {
+//      console.log("2ta musbat");
+// } else if (a<0&&b<0&&c>0||a<0&&b>0&&c<0){
+//      console.log("1ta musbat");
+// }
+// if 3
+// let a = 1
+// let b = 2
+// let c = 1
+// if (a>b&&a>c){
+//      console.log("a eng katta son");
+// } else if (b>a&&b>c){
+//      console.log("b eng katta son")
+// } else if (c>a&&c>b){
+//      console.log("c eng katta son")
+// }
+// if 4
+// let a = 1
+// let b = 1
+// let c = 2
+// if (c==b){
+//      console.log("a soni boshqa yani 1 chi paditsiya");
+// } else if (c==a){
+//      console.log("b soni boshqa yani 2 chi paditsiya");
+// } else if (a==b) {
+//      console.log("c soni boshqa yani 3 chi paditsiya");
+// }
+// if 5
+// let a = 7
+// if (a==2 || a==-2){
+//      console.log(a*2);
+// } else {
+//      console.log(-3*a);
+// }
+
+
+
+// if 1
+// let a = 2
+// if (a>0){
+//      console.log(a+1);
+// }
+
+// if 2
+// let a = 1
+// if (a>0){
+//      console.log(a+1);
+// } else if (a<0){
+//      console.log(a-2);
+// } else if (a==0){
+//      console.log(a+10);
+// }
+
+// if 3
+// let a = 2;
+// let b = 4;
+// let c = 5;
+// let counter = 0;
+
+
+// if (a > 0) {
+//     counter++;
+// }
+// if (b > 0) { 
+//     counter++;
+// }
+// if (c > 0) { 
+//     counter++;
+// }
+
+// console.log(counter); 
+
+// if 4
+// let a = 3
+// let b = 4
+// if (a>b){
+//      console.log(`${a} katta son`);
+// } else if (b>a) {
+//      console.log(`${b} katta son`);
+// }
+
+// if 5
+// let a = 4
+// let b = 2
+// if (a<b){
+//      console.log("a kichik son yani 1chi tartib raqamda");
+// } else if (b<a){
+//      console.log("b kichik son yani 2chi tartib raqamda ");
+// }
+
+// if 6
+// let a = 4
+// let b = 2
+// if (a>b){
+//      console.log("a katta son");
+// } else if (b>a) {
+//      console.log("b katta son");
+// }
+// if 6 
+// let a = 0
+// if (a>0&&a%2==1){
+//     console.log(" a soni musbat toq");
+// } else if (a>0&&a%2==0) {
+//     console.log(" a soni musbat juft");
+// } else if (a==0){
+//     console.log("a soni nolga teng");
+// } else if (a<0&&a%2==0) {
+//     console.log(" a soni manfiy juft");
+// } else if (a>0&&a%2==1) {
+//     console.log("a soni  manfiy toq");
+// }
+
+
+
+// if 8
+// let a = 2
+// let b = 1
+// if (a==b){
+//     console.log( ${a} = soni ${b} = soni);
+//     console.log(a+b);
+    
+// } else {
+//     console.log("0");
+    
+// }
+
+// if 9
+// let a = 2
+// let b = 1
+// if (a>b) {
+//     console.log(${a} katta son);
+    
+// } else if (b>a) {
+//     console.log(${b} katta son);
+    
+// } else if (a==b) {
+//     console.log(0 ${a} ${b});
+    
+// }
+// if 10
+// let a = 1
+// let b = 2
+// let c = 3
+// if (a<b&&a<c) {
+//     console.log("a eng kichik son");
+// } else if (b<a&&b<c) {
+//     console.log("b eng kichik son ");
+// } else if (c<a&&c<b) {
+//     console.log("c eng kichik son");
+// }
+
+// if 11
+// let a = 2
+// let b = 2
+// let c = 4
+// if (b<a&&a>c) {
+//     console.log("a ortancha son");
+// } else if (a<b&&b>c) {
+//     console.log("b ortancha son");
+// } else if (c<a&&c>b) {
+//     console.log("c ortacha son");
+// }
+// if 12
+// let a = 7;
+// let b = 12;
+// let c = 5;
+
+// if (a + b >= a + c && a + b >= b + c) {
+//     console.log("eng katta: a =", a, "va b =", b);
+// } else if (a + c >= a + b && a + c >= b + c) {
+//     console.log("eng katta: a =", a, "va c =", c);
+// } else {
+//     console.log("eng katta: b =", b, "va c =", c);
+// }
+
+// if 13
+// let a = 3;
+// let b = 5;
+// let c = 8;
+
+// if (a <= b && b <= c) {
+//     a *= 2;
+//     b *= 2;
+//     c *= 2;
+// } else {
+//     a = -a;
+//     b = -b;
+//     c = -c;
+// }
+
+// console.log(a, b, c,);
+// if 14 
+// let a = 7;
+// let b = 3;
+// let c = 5;
+
+// if (a <= b && b <= c) {
+//     a *= 2;
+//     b *= 2;
+//     c *= 2;
+// } else {
+//     a = -a;
+//     b = -b;
+//     c = -c;
+// }
+// console.log( a, b, c);
+// if 15 
+// let a = 3;
+// let b = 3;
+// let c = 3;
+// let d = 5;
+// if (b==c&&c==d&&b==d) {
+//      console.log("tartib raqami 1");
+// } else if (a==c&&d==c&&a==d) {
+//      console.log("tartib raqami 2");
+// } else if (a==b&&b==d&&a==d) {
+//      console.log("tartib raqami 3");
+// } else if (a==c&&b==c&&a==b){
+//      console.log("tartib raqami 4");
+// } else {
+//       console.log(" bu xato");
+// }
+
+// if 16 
+// let x = 8; 
+
+// if (x <= 0) {
+//     console.log(-x); 
+// } else if (x > 0 && x < 2) {
+//     console.log(x ** 2); 
+// } else if (x >= 2) {
+//     console.log(4); 
+// }
+
+// case 1
+// const kun = 5;
+
+// switch (kun) {
+//      case 1:
+//          console.log("dushanba");
+//          break;
+//      case 2:
+//          console.log("seshanba");
+//          break;
+//      case 3:
+//          console.log("chorshanba");
+//          break;
+//      case 4:
+//          console.log("payshanba");
+//          break;
+//      case 5:
+//          console.log("juma");
+//          break;
+//      case 6:
+//          console.log("shanba");
+//          break;
+//      case 7:
+//          console.log("yakshanba");
+//          break;
+//      default:
+//          console.log("Noto'g'ri kun raqami");
+//  }
+ 
+// case 2
+// const oy = 3
+// switch (oy) {
+//      case 1:
+//          console.log("qish");
+//          break;
+//      case 2:
+//          console.log("qish");
+//          break;
+//      case 3:
+//          console.log("bahor");
+//          break;
+//      case 4:
+//          console.log("bahor");
+//          break;
+//      case 5:
+//          console.log("bahor");
+//          break;
+//      case 6:
+//          console.log("yoz");
+//          break;
+//          case 1:
+//           console.log("yoz");
+//           break;
+//       case 7:
+//           console.log("yoz");
+//           break;
+//       case 8:
+//           console.log("chorshanba");
+//           break;
+//       case 9:
+//           console.log("payshanba");
+//           break;
+//       case 10:
+//           console.log("juma");
+//           break;
+//       case 11:
+//           console.log("shanba");
+//           break;
+//       case 12:
+//           console.log("qish");
+//           break;
+//      default:
+//          console.log("Noto'g'ri oy raqami");
+//  }
+
+//  case 3
+// const oy = 3;
+
+// switch (oy) {
+//     case 1:
+//     case 3:
+//     case 5:
+//     case 7:
+//     case 8:
+//     case 10:
+//     case 12:
+//         console.log("31 kun");
+//         break;
+//     case 4:
+//     case 6:
+//     case 9:
+//     case 11:
+//         console.log("30 kun");
+//         break;
+//     case 2:
+//         console.log("28 yoki 29 kun yilga bog'liq");
+//         break;
+//     default:
+//         console.log("Noto'g'ri oy raqami");
+// }
+
+// case 4
+// const birlik = 1;
+// const uzunlik = 150;
+
+// let natija;
+
+// switch (birlik) {
+//     case 1:
+//         natija = uzunlik * 0.1;
+//         break;
+//     case 2:
+//         natija = uzunlik * 1000;
+//         break;
+//     case 3:
+//         natija = uzunlik;
+//         break;
+//     case 4:
+//         natija = uzunlik * 0.001;
+//         break;
+//     case 5:
+//         natija = uzunlik * 0.01;
+//         break;
+//     default:
+//         console.log("Noto'g'ri birlik");
+//         break;
+// }
+
+// console.log(natija);
+// case 5
+// const birlik = 3; 
+// const ogirlik = 150;
+
+// let natija;
+
+// switch (birlik) {
+//     case 1:
+//         natija = ogirlik;
+//         break;
+//     case 2:
+//         natija = ogirlik * 0.000001;
+//         break;
+//     case 3:
+//         natija = ogirlik * 0.001;
+//         break;
+//     case 4:
+//         natija = ogirlik * 1000;
+//         break;
+//     case 5:
+//         natija = ogirlik * 0.1;
+//         break;
+//     default:
+//         console.log("Noto'g'ri birlik");
+//         break;
+// }
+
+// console.log(natija);
+
